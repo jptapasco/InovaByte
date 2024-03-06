92f063850d567bae50a4633922f39fbbb402e20e
jptapasco <restrepofreddy28@gmail.com>
2024-03-06 10:32:26 -0500

    limpiando rama

--- pool/resources/views/layouts/header.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <ul class="nav justify-content-end fixed-top bg-success py-2 text-light">
-        <li class="nav-item mx-4 my-2">
-            <i class="fa-solid fa-magnifying-glass"></i>
-        </li>
-    </ul>
-</div>
