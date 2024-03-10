3289839989268b5c5f058ba6ab585e87136c78c7
SrDamaez <david2013mip@gmail.com>
2024-03-09 20:19:21 -0500

    Terrorismo completado

--- pool/database/migrations/2024_02_23_044603_create_mesas_table.php
+++ pool/database/migrations/2024_02_23_044603_create_mesas_table.php
@@ -6,17 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('mesas', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('id_tipo_mesas');
             $table->unsignedBigInteger('id_mesera_asignada')->nullable();
+            $table->unsignedBigInteger('id_cliente_asignado')->nullable();
             $table->foreign('id_tipo_mesas')->references('id')->on('tipo_mesas');
             $table->foreign('id_mesera_asignada')->references('id')->on('users');
+            $table->foreign('id_cliente_asignado')->references('id')->on('users');
             $table->timestamps();
         });
     }
