6f8e9af85ab6b746da07e6ee809e735eb9036b01
SrDamaez <david2013mip@gmail.com>
2024-03-14 11:04:08 -0500

    Limpiando

--- pool/resources/views/components/primary-button.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['type' => 'button', 'iconL' => '', 'iconR' => '', 'color' => 'success'])
-
-<button {{ $attributes->merge(['type' => $type, 'class' => 'btn btn-' . $color]) }}>
-    @if ($iconL != '') <i class="{{ $iconL }}"></i> @endif 
-    {{ $slot }} 
-    @if ($iconR != '') <i class="{{ $iconR }}"></i> @endif
-</button>
